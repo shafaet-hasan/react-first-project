ce200c6cfdf87788451905d0c3bf026b88a0e175
Shafaet1722298642 <93768009+Shafaet1722298642@users.noreply.github.com>
2024-11-01 00:38:09 -0400

    created react project, initialized with tailwind CSS and loaded fake json data

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='text-9xl'>Knowledge Cafe</h1>
+    <Blogs></Blogs>
+
+    </>
+  )
+}
+
+export default App
